0646b1e0307cbe894ebf23aa274bfbb9f0add408
AHbelanger <hbelanger.antoine@gmail.com>
2019-02-22 18:43:21 -0500

    list all ads and they are clickable

--- src/containers/Browse.js
+++ src/containers/Browse.js
@@ -4,29 +4,47 @@ export default class Browse extends Component{
 
     state = {
         loading: true,
-        person: null,
+        spot: null,
     };
 
     async componentDidMount(){
        const url = "https://parking-system-ecse428.herokuapp.com/spot/all";
        const response = await fetch(url);
        var data = await response.json();
-       data = JSON.parse(JSON.stringify(data[1]));
+       data = JSON.parse(JSON.stringify(data));
        console.log(data)
-       this.setState({person: data, loading: false})
-       console.log(this.state.person)
+       this.setState({spot: data, loading: false})
+       console.log(this.state.spot)
     }
-
+    
+    displayAds=() => {
+        console.log("WAS CLICKED")
+    }
+    
     render(){
-        return <div>
-            {this.state.loading || !this.state.person ? (
-            <div>loading...</div> 
-            ) : (
-            <div>
-                <div>{this.state.person.street_Number}</div>
-                <div>{this.state.person.steet_Name}</div>
-            </div>
-            )}
-        </div>;
+        if(this.state.loading){
+            return (
+                <div>loading...</div>
+            );
+        }else{
+            return  this.state.spot.map((todo, index) => (
+                <div key={index}>
+                    <div onClick={this.displayAds()}>
+                        <h1>{todo.street_Number} {todo.steet_Name}</h1> 
+                        
+                    </div>
+                </div>
+            ));
+        }
+        // return <div>
+        //     {this.state.loading || !this.state.spot ? (
+        //     <div>loading...</div> 
+        //     ) : (
+        //     <div>
+        //         <div>{this.state.spot.street_Number}</div>
+        //         <div>{this.state.spot.steet_Name}</div>
+        //     </div>
+        //     )}
+        // </div>;
     }
 }
