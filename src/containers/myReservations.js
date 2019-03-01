f0a6f26b60dfd13be54c1b894396aabc49679cd1
mahad96 <mahad.khan@mail.mcgill.ca>
2019-03-01 10:50:12 -0500

    Functional cancel reservation

--- src/containers/myReservations.js
+++ src/containers/myReservations.js
@@ -2,44 +2,59 @@ import React,{Component} from "react";
 import axios from 'axios';
 
 export default class myReservations extends Component{
+    constructor(props) {
+        super(props);
 
-    state = {
-        loading: true,
-        reservation: null,
-    };
+        this.state = {
+            loading: true,
+            reservation: null,
+            pkey: null,
+        };
+    }
+
+
 
     async componentDidMount(){
        const url = "https://parking-system-ecse428.herokuapp.com/reservation/all";
        const response = await fetch(url);
-       var data = await response.json();
+       let data = await response.json();
        data = JSON.parse(JSON.stringify(data));
        console.log(data);
        this.setState({reservation: data, loading: false});
        console.log(this.state.reservation);
     }
 
-    cancelReservation= event => {
-      var values = [];
-      var keys = Object.keys(localStorage);
-      var i = keys.length;
-      while ( i-- ) {
-          values.push( localStorage.getItem(keys[i]) );
-      }
-      const user = JSON.parse(values[0]);
-        console.log(user);
-        console.log(String(user.last_Name));
+    /*async componentDidUpdate(prevProps) {
+        // Typical usage (don't forget to compare props):
+        if (this.props.userID !== prevProps.userID) {
+            this.fetchData(this.props.userID);
+            const url = "https://parking-system-ecse428.herokuapp.com/reservation/all";
+            const response = await fetch(url);
+            let data = await response.json();
+            data = JSON.parse(JSON.stringify(data));
+            console.log(data);
+            this.setState({reservation: data, loading: false});
+        }*/
 
-     /*   var headers = {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        }
 
-          axios({
-            method: 'post',
-            url: 'https://parking-system-ecse428.herokuapp.com/reservation',
-            data: user,
-            headers: headers
-          })*/
+
+    cancelReservation(pkey) {
+
+        axios.delete(`/reservation/delete/${pkey}`)
+            .then((function (response){
+                if(response.status == 200){
+
+                    localStorage.setItem('myData', JSON.stringify(response.data));
+                    this.render();
+
+                }
+            }))
+        //this.render();
+
+
+
+
+
     }
     divStyle = {
         backgroundColor: '#A9A8E8',
@@ -48,9 +63,9 @@ export default class myReservations extends Component{
     }
 
     getUser(){
-        var values = [];
-        var keys = Object.keys(localStorage);
-        var i = keys.length;
+        let values = [];
+        let keys = Object.keys(localStorage);
+        let i = keys.length;
         while ( i-- ) {
             values.push( localStorage.getItem(keys[i]) );
         }
@@ -59,8 +74,8 @@ export default class myReservations extends Component{
     }
 
     reservationList() {
-        var user = this.getUser();
-        var reservations = user.reservations;
+        let user = this.getUser();
+        let reservations = user.reservations;
         if (reservations.length > 0) {
             var list = reservations.map((todo, index) => (
                 <div key={index}>
@@ -68,22 +83,22 @@ export default class myReservations extends Component{
                         <div style={this.divStyle}>
                             <h3> Parking {todo.pkey} </h3>
                             <hr></hr>
-                            <p> Address :{todo.parkingSpot.street_Number}{todo.parkingSpot.steet_Name}</p>
-                            <p> Postal Code: {todo.parkingSpot.postal_Code}</p>
-                            <p> Start Date: {todo.start_Date}</p>
-                            <p> End Date: {todo.end_Date}</p>
-                            <p> Start Time: {todo.start_Time}</p>
-                            <p> End Time: {todo.end_Time}</p>
-                            <p> Total Cost: {todo.parkingSpot.current_Price}</p>
-                            <p> Rating : {todo.parkingSpot.avg_Rating}</p>
-                            <button onClick={this.cancelReservation}> Cancel</button>
+                            <p> Address: {todo.parkingSpot.street_Number} {todo.parkingSpot.steet_Name} </p>
+                            <p> Postal Code: {todo.parkingSpot.postal_Code} </p>
+                            <p> Start Date: {todo.start_Date} </p>
+                            <p> End Date: {todo.end_Date} </p>
+                            <p> Start Time: {todo.start_Time} </p>
+                            <p> End Time: {todo.end_Time} </p>
+                            <p> Total Cost: {todo.price_Paid} </p>
+                            <p> Rating: {todo.parkingSpot.avg_Rating} </p>
+                            <button onClick={this.cancelReservation(todo.pkey)}> Cancel </button>
                             </div>
                         </div>
                     </div>
                 ))
             }
         else{
-            list =  <div>You have no reservations</div>
+            list =  <div>You have no reservations.</div>
         }
         return list;
     }
