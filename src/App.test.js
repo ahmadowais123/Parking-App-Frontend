3a9157cee1ecd9bc9105e836f6aa3a673ec7b164
s1o3urn <d4vid4pplepie@gmail.com>
2019-03-22 18:34:50 -0400

    create an advertisement test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// Requirements
+require('chromedriver');
+const assert = require('assert');
+const {Builder, Key, By, until} = require('selenium-webdriver');
+
+describe('Checkout Parking App', function () {
+    let driver;
+    before(async function() {
+        driver = await new Builder().forBrowser('chrome').build();
+    });
+
+    // Test steps
+    it('A Seller creates an advertisement'), async function() {
+        //Go to website
+        // log in as a Seller
+        // create an advertisement
+    }
+}
+
+/*
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+*/
